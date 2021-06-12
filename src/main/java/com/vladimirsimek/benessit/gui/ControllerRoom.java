3aeb64cfc62f2014430b3587307eb2b180f787a6
Vladimir <minervladik@gmail.com>
2021-06-12 22:46:14 +0200

    hi

--- src/main/java/com/vladimirsimek/benessit/gui/ControllerRoom.java
+++ src/main/java/com/vladimirsimek/benessit/gui/ControllerRoom.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
@@ -27,6 +28,10 @@ public class ControllerRoom {
     public int a = 0;
     @FXML
     public ImageView lobby;
+    @FXML
+    public ProgressBar asianWomanBar;
+    @FXML
+    public Button fightButton;
 
     public void goUp(ActionEvent actionEvent) {
         a = 10;
@@ -39,13 +44,21 @@ public class ControllerRoom {
     }
 
     public void goLeft(ActionEvent actionEvent) {
-        a = -5;
-        reformRoom(a);
+        if (a == -5) {
+            roomName.setText("YOU WON!");
+            roomTitle.setText("You killed her!");
+            RoomDescription.setText("You finally killed that asian woman. GG!");
+            StackPane.setMargin(RoomDescription, new Insets(0, 950, 600, 0));
+        } else {
+            a = -5;
+            reformRoom(a);
+        }
     }
 
     public void goRight(ActionEvent actionEvent) {
         a = 5;
         reformRoom(a);
+
     }
 
     public void hide() {
@@ -54,6 +67,12 @@ public class ControllerRoom {
         goLeftButton.setOpacity(0);
         goRightButton.setOpacity(0);
         lobby.setOpacity(0);
+        fightButton.setOpacity(0);
+    }
+
+    public void hideUpDown() {
+        goDownButton.setOpacity(0);
+        goUpButton.setOpacity(0);
     }
 
     public void reformRoom(int a) {
@@ -62,25 +81,42 @@ public class ControllerRoom {
             roomName.setText("ELEVATOR");
             roomTitle.setText("WOW! You died dude!!");
             RoomDescription.setText("A bunch of black people stole your money and lungs.");
-            StackPane.setMargin(RoomDescription, new Insets(0,800,600,0));
+            StackPane.setMargin(RoomDescription, new Insets(0, 800, 600, 0));
         } else if (a == -10) {
             hide();
             roomName.setText("OUTSIDE");
             roomTitle.setText("WOW! You died dude!!");
             RoomDescription.setText("An army convoy was passing by and ran over you.");
-            StackPane.setMargin(RoomDescription, new Insets(0,800,600,0));
+            StackPane.setMargin(RoomDescription, new Insets(0, 800, 600, 0));
         } else if (a == 5) {
             hide();
             roomName.setText("RECEPTION");
             roomTitle.setText("WOW! You died dude!!");
             RoomDescription.setText("Secretary thought that you are robber because you are black and shot you.");
-            StackPane.setMargin(RoomDescription, new Insets(0,200,600,0));
+            StackPane.setMargin(RoomDescription, new Insets(0, 200, 600, 0));
         } else {
             hide();
+            asianWomanBar.setProgress(1);
+            asianWomanBar.setVisible(true);
+            fightButton.setVisible(true);
+            fightButton.setOpacity(1);
             roomName.setText("TOILET");
-            roomTitle.setText("WOW! You died dude!!");
-            RoomDescription.setText("An Asian woman stabbed you because you have an LGBT shirt, she hates gay people.");
-            StackPane.setMargin(RoomDescription, new Insets(0,130,600,0));
+            roomTitle.setText("OMFG AN ASIAN WOMAN!!");
+            RoomDescription.setText("Boss fight! Kill na asian woman!!");
+            StackPane.setMargin(RoomDescription, new Insets(0, 1250, 600, 0));
         }
+    }
+
+    public void fight(ActionEvent actionEvent) {
+        if (asianWomanBar.getProgress() <= 0.21) {
+            goLeftButton.setOpacity(1);
+            goRightButton.setOpacity(1);
+            asianWomanBar.setOpacity(0);
+            fightButton.setOpacity(0);
+        } else {
+            asianWomanBar.setProgress(asianWomanBar.getProgress() - 0.2);
+        }
+        System.out.println(asianWomanBar.getProgress());
+
     }
 }
